8c11873f155f802e639f710684847e07d6eb216c
pkulik0 <pkulik0@icloud.com>
2022-08-17 21:41:30 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# chip8-emulator
+
+## My implementation of the CHIP8 Virtual Machine. Written in C++
---
+++ src/main.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main() {
+    std::cout << "Chip8 Emulator Init" << std::endl;
+    return 0;
+}
