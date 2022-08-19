d02c81757f1dc606810cb16b6aab51296d9dae59
pkulik0 <pkulik0@icloud.com>
2022-08-19 20:45:36 +0200

    Implement almost all opcodes. Corax89'S test passed

--- src/chip8.cxx
+++ src/chip8.cxx
@@ -5,9 +5,15 @@
 #include <chrono>
 
 #include "../include/chip8.hxx"
+#include "../include/font.hxx"
 
 Chip8::Chip8() : index_register{}, registers{}, memory{}, stack{}, delay_timer{}, sound_timer{}, pc{}, framebuffer{} {
+    srand(time(0));
     memory[0x1FF] = 2;
+
+    for(size_t i = 0; i < font.size(); i++) {
+        memory[0xF0+i] = font[i];
+    }
 }
 
 Chip8::~Chip8() {}
@@ -23,13 +29,13 @@ inline void Chip8::increment_pc() {
 void Chip8::load(std::vector<uint8_t>& program) {
     reset_pc();
 
-    for(auto i = 0; i < program.size(); i++) {
+    for(size_t i = 0; i < program.size(); i++) {
         memory[CH8_PC_START+i] = program[i];
     }
 }
 
 void Chip8::clear_fb() {
-    for(uint16_t addr = 0; addr < framebuffer.size(); addr++) {
+    for(size_t addr = 0; addr < framebuffer.size(); addr++) {
         framebuffer[addr] = CH8_SCREEN_COLOR_2;
     }
 }
@@ -63,12 +69,12 @@ void Chip8::decode_instruction(uint16_t& instruction) {
             }
             break;
         }
+        case 0x2: {
+            stack.push(pc);
+        }
         case 0x1: {
             pc = value12;
             break;
-        }
-        case 0x2: {
-            stack.push(pc);
         }
         case 0x3: {
             if(registers[reg_x] == value8) {
@@ -96,6 +102,52 @@ void Chip8::decode_instruction(uint16_t& instruction) {
             registers[reg_x] += value8;
             break;
         }
+        case 0x8: {
+            switch(value4) {
+                case 0x0: {
+                    registers[reg_x] = registers[reg_y];
+                    break;
+                }
+                case 0x1: {
+                    registers[reg_x] |= registers[reg_y];
+                    break;
+                }
+                case 0x2: {
+                    registers[reg_x] &= registers[reg_y];
+                    break;
+                }
+                case 0x3: {
+                    registers[reg_x] ^= registers[reg_y];
+                    break;
+                }
+                case 0x4: {
+                    registers[reg_x] += registers[reg_y];
+                    // overflow check
+                    break;
+                }
+                case 0x5: {
+                    registers[reg_x] -= registers[reg_y];
+                    // same here
+                    break;
+                }
+                case 0x7: {
+                    registers[reg_y] -= registers[reg_x];
+                    // same here
+                    break;
+                }
+                case 0x6: {
+                    registers[0xF] = registers[reg_x] & 1;
+                    registers[reg_x] = registers[reg_x] >> 1;
+                    break;
+                }
+                case 0xE: {
+                    registers[0xF] = registers[reg_x] >> 7;
+                    registers[reg_x] = registers[reg_x] << 1;
+                    break;
+                }
+            }
+            break;
+        }
         case 0x9: {
             if(registers[reg_x] != registers[reg_y]) {
                 increment_pc();
@@ -104,6 +156,14 @@ void Chip8::decode_instruction(uint16_t& instruction) {
         }
         case 0xA: {
             index_register = value12;
+            break;
+        }
+        case 0xB: {
+            pc = value12 + registers[0];
+            break;
+        }
+        case 0xC: {
+            registers[reg_x] = rand() & value8;
             break;
         }
         case 0xD: {
@@ -130,15 +190,71 @@ void Chip8::decode_instruction(uint16_t& instruction) {
             }
             break;
         }
+        case 0xE: {
+            switch(value8) {
+                case 0x9E: {
+                    // key pressed
+                    break;
+                }
+                case 0xA1: {
+                    // key not pressed
+                    break;
+                }
+            }
+            break;
+        }
         case 0xF: {
-            uint16_t addr = index_register;
-            for(auto i = 0; i <= reg_x; i++) {
-                registers[i] = memory[addr++];
+            switch(value8) {
+                case 0x07: {
+                    registers[reg_x] = delay_timer;
+                    break;
+                }
+                case 0x0A: {
+                    // block
+                    pc -= CH8_PC_STEP;
+                    break;
+                }
+                case 0x15: {
+                    delay_timer = registers[reg_x];
+                    break;
+                }
+                case 0x18: {
+                    sound_timer = registers[reg_x];
+                    break;
+                }
+                case 0x1E: {
+                    index_register = registers[reg_x];
+                    break;
+                }
+                case 0x29: {
+                    index_register = memory[0xF0 + registers[reg_x]*0x05];
+                    break;
+                }
+                case 0x33: {
+                    uint8_t num = registers[reg_x];
+                    for(auto i = 2; i >= 0; i--) {
+                        memory[index_register+i] = num % 10;
+                        num = num / 10;
+                    }
+                    break;
+                }
+                case 0x55: {
+                    for(auto i = 0; i <= reg_x; i++) {
+                        memory[index_register+i] = registers[i];
+                    }
+                    break;
+                }
+                case 0x65: {
+                    for(auto i = 0; i <= reg_x; i++) {
+                        registers[i] = memory[index_register+i];
+                    }
+                    break;
+                }
             }
             break;
         }
         default: {
-            printf("\tnot implemented: %x, full: %x\n", instruction_type, instruction);
+            printf("\tunknown instruction: %x, full: %x\n", instruction_type, instruction);
             break;
         }
     }
