697029b46b8bd543316e9d4b1a2741de3596dbb2
pkulik0 <pkulik0@icloud.com>
2022-08-22 22:20:38 +0200

    Remove the hard-coded test image, get filename from args

--- src/main.cxx
+++ src/main.cxx
@@ -2,11 +2,18 @@
 
 #include "../include/launcher.hxx"
 
-int main() {
+int main(int argc, char** argv) {
     srand(time(0));
 
+    if(argc < 2) {
+        std::cout << "Usage:\n";
+        std::cout << argv[0] << " <path_to_image>\n" << std::endl;
+        return 1;
+    }
+    std::string filename{argv[1]};
+
     Launcher launcher{"chip8-emulator", 640, 320};
-    launcher.run("chip8-test-suite.ch8");
+    launcher.run(filename);
 
     return 0;
 }
